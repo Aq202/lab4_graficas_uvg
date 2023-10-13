2be151b4a8dd6a22d548cbab35fe2057905f53a4
Diego <diegoguatedb2002@gmail.com>
2023-10-13 12:21:36 -0600

    Shader planeta 3

--- src/shaders.h
+++ src/shaders.h
@@ -84,7 +84,7 @@ Fragment fragmentShader(Fragment& fragment) {
 
 */
 
-
+/*
 Fragment fragmentShader(Fragment& fragment) {
     
 
@@ -120,6 +120,46 @@ Fragment fragmentShader(Fragment& fragment) {
     else if (noise > 0.6 && noise < 0.65) tempColor *= 1.06f;
 
     Color color = Color(tempColor.x, tempColor.y, tempColor.z);
+
+    fragment.color = color * fragment.intensity;
+
+    return fragment;
+}
+
+*/
+
+
+Fragment fragmentShader(Fragment& fragment) {
+    Color color;
+
+    glm::vec3 purple = glm::vec3(91.0f, 8.0f, 136.0f);
+    glm::vec3 pink = glm::vec3(242.0f, 4.0f, 127.0f);
+    glm::vec3 blue = glm::vec3(53.0f, 52.0f, 158.0f);
+    glm::vec3 aqua = glm::vec3(24.0f, 116.0f, 132.0f);
+    glm::vec3 green = glm::vec3(14.0f, 206.0f, 58.0f);
+    glm::vec3 yellow = glm::vec3(221.0f, 231.0f, 6.0f);
+
+    glm::vec2 uv = glm::vec2(fragment.original.x, fragment.original.y);
+
+    FastNoiseLite noiseGenerator;
+    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+
+    float offset_x = 8000.0f;
+    float offset_y = 8000.0f;
+    float zoom = 200.0f;
+
+    float noise = noiseGenerator.GetNoise((uv.x + offset_x) * zoom, (uv.y + offset_y) * zoom);
+    //SDL_Log("%f", noiseValue);
+
+    glm::vec3 tmpColor = pink;
+
+    if(noise > 0.8) tmpColor = purple;
+    else if (noise < 0.7 && noise > 0.63) tmpColor = yellow;
+    else if (noise < 0.55 && noise > 0) tmpColor = green;
+    else if (noise < 0 && noise > -0.3) tmpColor = aqua;
+    else if (noise < -0.5 && noise > -0.8) tmpColor = blue;
+
+    color = Color(tmpColor.x, tmpColor.y, tmpColor.z);
 
     fragment.color = color * fragment.intensity;
 
