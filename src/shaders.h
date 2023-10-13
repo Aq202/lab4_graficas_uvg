186394cd4285ef314821860340838990ff3c157c
Diego <diegoguatedb2002@gmail.com>
2023-10-13 13:12:32 -0600

    Shader planeta 4

--- src/shaders.h
+++ src/shaders.h
@@ -128,7 +128,7 @@ Fragment fragmentShader(Fragment& fragment) {
 
 */
 
-
+/*
 Fragment fragmentShader(Fragment& fragment) {
     Color color;
 
@@ -165,3 +165,37 @@ Fragment fragmentShader(Fragment& fragment) {
 
     return fragment;
 }
+*/
+
+Fragment fragmentShader(Fragment& fragment) {
+    Color color = Color(59, 79, 188);
+
+    // Coordenadas de textura UV
+    glm::vec2 uv = glm::vec2(fragment.original.x, fragment.original.y);
+
+    for(int i = 1; i <= 3; i++){
+
+    
+
+    // Parámetros de ruido
+    float offset_x = 2000 * i;
+    float offset_y = 6000 * i;
+    float zoom = 200.0;
+
+    FastNoiseLite noiseGenerator;
+    noiseGenerator.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2);
+
+    // Genera el valor de ruido en función de las coordenadas de textura y el tiempo
+    float noise = noiseGenerator.GetNoise((uv.x + offset_x) * zoom, (uv.y + offset_y) * zoom);
+
+        if(i == 1 && noise > 0.5) color = Color(210, 239, 240);
+        if(i == 2 && noise > 0.7) color = Color(216, 195, 145);
+        if(i == 3 && (noise > 0.8) ) color = Color(194, 114, 6);
+
+    }
+
+    fragment.color = color * fragment.intensity;
+
+    return fragment;
+}
+
